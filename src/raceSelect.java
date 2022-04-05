a59310e1639411e8236eb124e165d82ff42abd9c
MaxSorg <94195077+MaxSorg@users.noreply.github.com>
2022-04-05 00:38:54 -0400

    final updates before migrating

--- src/raceSelect.java
+++ src/raceSelect.java
@@ -1,17 +1,17 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class raceSelect extends Application {
 
     @Override
     public void start(Stage testStage) throws Exception {
-        testStage.setTitle("Does it work? :O");
-        
-        Label label = new Label(PlayerClass.WIZARD.getAbilityOne() + "\n" + 
-        PlayerClass.WIZARD.abilityOneDescription());
-        Scene scene = new Scene(label, 400, 200);
+        testStage.setTitle("My Awesome Character Creator");  
+        Text title = new Text("Character Creator");     
+        Vbox vbox = new Vbox
+        Scene scene = new Scene(vbox);
         testStage.setScene(scene);
         
         testStage.show();
